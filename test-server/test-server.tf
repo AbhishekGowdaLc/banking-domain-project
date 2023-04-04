eb8f24caf5c8e0c424b5dfc3b290f86d485c9247
AbhishekGowdaLc <84676949+AbhishekGowdaLc@users.noreply.github.com>
2023-04-04 18:56:21 +0530

    Update test-server.tf

--- test-server/test-server.tf
+++ test-server/test-server.tf
@@ -1,38 +1,36 @@
-resource "aws_eip_association" "eip_assoc"{
-  instance_id = aws_instance.test-server.id
+resource "aws_eip_association" "eip_assoc" {
+  instance_id   = aws_instance.test-server.id
   allocation_id = "eipalloc-0d2089363c0bfe59b"
-}	
-	
+}
+resource "aws_instance" "test-server" {
+  ami           = "ami-00c39f71452c08778" 
+  instance_type = "t2.micro" 
+  key_name = "abhishek"
+  vpc_security_group_ids= ["sg-0d42aaa78deb47d92"]
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    private_key = file("./abhishek.pem")
+    host     = self.public_ip
+  }
+  provisioner "remote-exec" {
+    inline = [ "echo 'wait to start instance' "]
+  }
+  tags = {
+    Name = "test-server"
+  }
+ provisioner "local-exec" {
 
-resource "aws_instance" "test-server"{
- ami = "ami-00c39f71452c08778"
- instance_type = "t2.micro"
- key_name = "abhishek"
- vpc_security_group_ids = ["sg-0d42aaa78deb47d92"]
- availability_zone = "us-east-1a"
-   connection {
-        type = "ssh"
-	user = "ec2-user" 
-	private_key = file("./abhishek.pem")
-	host = self.public_ip
-	}
-provisioner "remote-exec"{
-   inline = ["echo 'started remote exec'"]
+        command = " echo ${aws_instance.test-server.public_ip} > inventory "
+ }
+ 
+ provisioner "local-exec" {
+ command = "ansible-playbook /var/lib/jenkins/workspace/project-02-banking/test-server/test-server-playbook.yml "
+  } 
 }
 
-tags = {
-  Name = "test-server"
-}
+output "test-server_public_ip" {
 
-provisioner "local-exec"{
-command = "echo $(aws_instance.test-server.public_ip) > inventory"
-}
-
-
-provisioner "local-exec"{
-  command = "ansible-playbook /var/lib/jenkins/workspace/banking-project/test-server/test-server-playbook.yml"
-}
-}
-output "ip" {
   value = aws_eip_association.eip_assoc.public_ip
+  
 }
